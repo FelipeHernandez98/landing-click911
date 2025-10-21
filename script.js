4e83bb4c8192e86ebdbdb14dfc7122f9317317a4
Andres Felipe Hernandez Caicedo <afelipehernandezdev@gmail.com>
2025-10-21 15:26:54 -0500

    Primer commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// Efecto de scroll suave para enlaces
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
